12e1a0d3f93c7c814dd636ad7e4512e7e4cabc98
nealgandhi <gandhi.neal2002@gmail.com>
2022-09-12 17:19:59 -0500

    Initial Content Ideas

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ import "./input.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-white">Hello</h1>
+      <div>
+        <h1 className="font-semibold text-xl">Neal Gandhi</h1>
+        <h1> Computer Science Student @ UTDallas 2024</h1>
+        <a href="https://github.com/nealgandhi/">github.com/nealgandhi</a>
+        <p>
+          Hi. I'm Neal. I'm a Junior studying Computer Science at the University
+          of Texas at Dallas. I have an interest in Full Stack Development and
+          Embedded Software Engineering.
+        </p>
+        {/* insert waving emoji after hi -- figure that out */}
+      </div>
+      <div>
+        <h1>Here are some of my projects and past experiences:</h1>
+        <ul>
+          <li>Capital One Software Engineering Intern Summer 2022</li>
+          <li>Accenture Data Engineering Intern Summer 2021</li>
+        </ul>
+      </div>
     </div>
   );
 }

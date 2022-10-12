495ae1e22ba36ac2ed8be6252d448cb98bb7962d
nealgandhi <gandhi.neal2002@gmail.com>
2022-10-12 12:02:38 -0500

    Adding important links and files

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import "./input.css";
 function App() {
   return (
     <div class="h-screen flex item-center justify-center dark:bg-gray-900 dark:text-gray-300">
-      <div className="my-auto mx-4 text-center p-6 sm:p-8 grid gap-2 bg-gradient-to-b sm:bg-gradient-to-r from-indigo-200 to-blue-300 shadow-2xl dark:from-gray-700 dark:to-gray-800 rounded-3xl">
+      <div className="my-auto mx-64 text-center p-6 sm:p-8 grid gap-2 bg-gradient-to-b sm:bg-gradient-to-r from-indigo-200 to-blue-300 shadow-2xl dark:from-gray-700 dark:to-gray-800 rounded-3xl">
         <div className="flex flex-row justify-center">
           <img
-            className="rounded-full mb-2 sm:my-auto mx-auto"
+            className="rounded-lg mb-2 sm:my-auto mx-auto"
             src="./NealGPhoto.jpeg"
             width={"25%"}
             height={"25%"}
@@ -21,14 +21,14 @@ function App() {
           <h1> CS @ UTDallas</h1>
         </div>
         <div className="flex flex-col justify-center">
-          <p className="px-[25vw]">
+          <p className="px-[8vw] py-1">
             I'm a Junior studying Computer Science at the University of Texas at
             Dallas. I have an interest in Full Stack Development and Embedded
             Software Engineering.
           </p>
         </div>
         <div>
-          <ul className="inline-grid grid-cols-2">
+          <ul className="inline-grid grid-cols-2 ">
             <li>Capital One Software Engineering Intern</li>{" "}
             <li>Summer 2022</li>
             <li>Accenture Data Engineering Intern</li> <li>Summer 2021</li>
@@ -41,14 +41,39 @@ function App() {
             {/* each will lead to a sub section/expand into more information - or have a resume type blurb */}
           </ul>
         </div>
-        <ul className="inline-grid grid-cols-4 leading-snug">
-          <li>
-            <a href="https://linkedin.com/in/neal-gandhi">Linkedin</a>
-          </li>
-          <li>Github</li>
-          <li>Resume</li>
-          <li>Email</li>
-        </ul>
+        <div className="item-center justify-center">
+          <ul className="inline-grid grid-cols-4 gap-8 leading-snug justify-center object-center">
+            <li>
+              <a href="https://linkedin.com/in/neal-gandhi">
+                <img
+                  src="./linkedin-logo.png"
+                  className="object-contain h-10 w-10"
+                ></img>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/nealgandhi/">
+                <img
+                  src="./github-sign.png"
+                  className="object-contain h-10 w-10"
+                ></img>
+              </a>
+            </li>
+            <li>
+              <a href="./neal_gandhi_resume.pdf">
+                <img src="./cv.png" className="object-contain h-10 w-10"></img>
+              </a>
+            </li>
+            <li>
+              <a href="mailto:gandhi.neal2002@gmail.com">
+                <img
+                  src="./email.png"
+                  className="object-contain h-10 w-10"
+                ></img>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
